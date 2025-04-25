c28495a21dcc17279835421a77d74d679ad95d80
lazaaq <lana.saiful.aqil@mail.ugm.ac.id>
2025-04-25 14:26:34 +0700

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const responses = JSON.parse(fs.readFileSync('./responses.json', 'utf-8'));
+
+app.post('/api/chat', (req, res) => {
+  const userMessage = req.body.message;
+  const reply = responses[userMessage] || "Sorry, I don't understand that. Try asking something else!";
+  res.json({ reply });
+});
+
+app.get('/api/questions', (req, res) => {
+    const questions = Object.keys(responses);
+    res.json({ questions });
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
